16516cff1a92aae17df41556b006a5b00ca9daff
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2020-03-28 22:21:18 +0100

    with map component

---
+++ frontend/src/components/StoreMapComponent.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import {initialize} from "../functions/google"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+export const StoreMapComponent = withScriptjs(
+  withGoogleMap((props: any) => {
+    useEffect(() => {
+      initialize();
+    });
+    return (
+        <div id="map">
+      <GoogleMap defaultZoom={8} defaultCenter={{ lat: -50.857, lng: 4.348 }}>
+        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+      </GoogleMap>
+        </div>
+    );
+  })
+);
--- frontend/src/screens/LocationScreen.tsx
+++ frontend/src/screens/LocationScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {StoreMapComponent} from "../components/StoreMapComponent"
 
 /**
  * TODO
@@ -11,6 +12,12 @@ const LocationScreen = () => {
   return (
     <article id='content'>
         Location
+        <StoreMapComponent
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCow4NONLwTfk6aJyDGYqExE9Y2YVOVm2I&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        ></StoreMapComponent>
     </article>
   )
 }
