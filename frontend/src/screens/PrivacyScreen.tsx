3ef3d8d6501527653928250ab1f8265000871723
e2f9o13y <62790292+e2f9o13y@users.noreply.github.com>
2020-03-29 10:40:58 +0200

    format mistakes in previosu commit

--- frontend/src/screens/PrivacyScreen.tsx
+++ frontend/src/screens/PrivacyScreen.tsx
@@ -14,18 +14,18 @@ const PrivacyScreen = () => {
         buttonText='Accept'
         message="{
                  
-                 <p>Hack the crisis built the Spread The Queue app as an Open Source app. This SERVICE is provided by Hack the crisis at no cost and is intended for use as is.</p>
+                <p>Hack the crisis built the Spread The Queue app as an Open Source app. This SERVICE is provided by Hack the crisis at no cost and is intended for use as is.</p>
 <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
 <p>If you choose to use our Service, then you agree to the use of information in relation to this policy. The Personal Information is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
 <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Spread The Queue unless otherwise defined in this Privacy Policy.</p>
 <h1 id="information-collection-and-used">Information Collection and Used</h1>
 <p>For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request will be retained on your device and is not collected by me in any way.
 The app does use third party services that may collect information used to identify you.
-Link to privacy policy of third party service providers used by the app
-Google Play Services
-Log Data</p>
+Link to privacy policy of third party service providers used by the app</p>
+<p>Google Play Services</p>
+<h1 id="log-data">Log Data</h1>
 <p>Whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.</p>
-<p>#Cookies</p>
+<h1 id="cookies">Cookies</h1>
 <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device&#39;s internal memory.
 This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
 Service Providers</p>
